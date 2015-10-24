14f70c2d3120c6e9f3c13c6810eddd34937640ac
Xenon156 <gosha56good@gmail.com>
2015-10-25 00:45:02 +0300

    Test commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QMessageBox messageBox;
+    messageBox.critical(0,"Igor","I can commit Project !");
+    messageBox.setFixedSize(500,200);
     w.show();
 
     return a.exec();
