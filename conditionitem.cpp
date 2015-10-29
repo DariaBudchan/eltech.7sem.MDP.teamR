a088f3642a016a709adf5ebfbe2cbf9be930547b
DariaBudchan <daria.budchan@mail.ru>
2015-10-29 20:02:59 +0300

    conditionCircleLogic

--- conditionitem.cpp
+++ conditionitem.cpp
@@ -1,18 +1,84 @@
 #include "conditionitem.h"
+#include "diagramscene.h"
+
 
 ConditionItem::ConditionItem(QGraphicsItem *parent)
     :QGraphicsEllipseItem(parent)
 {
     this->setRect(-20,-20,40,40);
-    this->setBrush(QColor::fromRgbF(0.2,0.2,0.2));
+    this->setBrush(QColor::fromRgbF(0.9,0.9,0.9));
     this->setPen(QPen(QColor::fromRgbF(0,0,0)));
     setFlag(QGraphicsItem::ItemIsMovable, true);
-    setFlag(QGraphicsItem::ItemIsSelectable, false);
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
     setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+}
+
+void ConditionItem::increaseValue()
+{
+    current_value++;
+}
+
+void ConditionItem::decreaseValue()
+{
+    if(current_value != 0){
+        current_value--;
+    }
+}
+
+void ConditionItem::setValue(unsigned int value)
+{
+    current_value = value;
+}
+
+unsigned int ConditionItem::getValue()
+{
+    return current_value;
+}
+
+void ConditionItem::removeArrow(ArrowItem *arrow)
+{
+    int index = arrows.indexOf(arrow);
+
+    if (index != -1)
+        arrows.removeAt(index);
+}
+
+void ConditionItem::removeArrows()
+{
+    //must be ended
+}
+
+void ConditionItem::addArrow(ArrowItem *arrow)
+{
+    arrows.append(arrow);
+}
+
+void ConditionItem::updateItem()
+{
+    foreach (ArrowItem *arrow, arrows) {
+        arrow->updatePosition();
+    }
 }
 
 ConditionItem::~ConditionItem()
 {
 
+}
+
+QVariant ConditionItem::itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value)
+{
+    if (change == QGraphicsItem::ItemPositionChange) {
+        foreach (ArrowItem *arrow, arrows) {
+            arrow->updatePosition();
+        }
+    }
+
+    if(change == QGraphicsItem::ItemSelectedHasChanged){
+        if(DiagramScene* d_scene = dynamic_cast<DiagramScene*>(this->scene())){
+            d_scene->itemChanged(this);
+        }
+    }
+
+    return value;
 }
 
--- conditionitem.h
+++ conditionitem.h
@@ -9,12 +9,32 @@
 #include <QGraphicsItem>
 #include <QPainter>
 
+#include "arrowitem.h"
+
 class ConditionItem : public QGraphicsEllipseItem
 {
     enum {Type = UserType+2};
+    unsigned int current_value;
+    QList<ArrowItem*> arrows;
 public:
     ConditionItem(QGraphicsItem* parent = 0);
+    int type() const Q_DECL_OVERRIDE {return Type;}
+    void increaseValue();
+    void decreaseValue();
+    void setValue(unsigned int value);
+    unsigned int getValue();
+
+    void removeArrow(ArrowItem* arrow);
+    void removeArrows();
+    void addArrow(ArrowItem* arrow);
+
+    void updateItem();
+
     ~ConditionItem();
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) Q_DECL_OVERRIDE;
+signals:
+    void selectedChange(QGraphicsItem* item);
 };
 
 #endif // CONDITIONITEM_H
