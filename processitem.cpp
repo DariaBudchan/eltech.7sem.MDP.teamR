4e7c2f97025db049c8ab36f244e61cb5d362edce
Xenon156 <gosha56good@gmail.com>
2015-11-04 02:56:05 +0300

    Rectangle functions
    Added simple object functions, which help control it.

--- processitem.cpp
+++ processitem.cpp
@@ -1,4 +1,5 @@
 #include "processitem.h"
+#include "diagramscene.h"
 
 ProcessItem::ProcessItem(QGraphicsItem *parent)
     :QGraphicsRectItem (parent)
@@ -14,5 +15,73 @@ ProcessItem::ProcessItem(QGraphicsItem *parent)
 ProcessItem::~ProcessItem()
 {
 
+}
+
+void ProcessItem::increaseValue()
+{
+    current_value++;
+}
+
+void ProcessItem::decreaseValue()
+{
+    if(current_value != 0){
+        current_value--;
+    }
+}
+
+void ProcessItem::setValue(unsigned int value)
+{
+    current_value = value;
+}
+
+unsigned int ProcessItem::getValue()
+{
+    return current_value;
+}
+
+void ProcessItem::removeArrow(ArrowItem *arrow)
+{
+    int index = arrows.indexOf(arrow);
+
+    if (index != -1)
+        arrows.removeAt(index);
+}
+
+void ProcessItem::removeArrows()
+{
+    foreach (ArrowItem *arrow, arrows) {
+        arrow->disconnect();
+        scene()->removeItem(arrow);
+        delete arrow;
+    }
+}
+
+void ProcessItem::addArrow(ArrowItem *arrow)
+{
+    arrows.append(arrow);
+}
+
+void ProcessItem::updateItem()
+{
+    foreach (ArrowItem *arrow, arrows) {
+        arrow->updatePosition();
+    }
+}
+
+QVariant ProcessItem::itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value)
+{
+    if (change == QGraphicsItem::ItemPositionChange) {
+        foreach (ArrowItem *arrow, arrows) {
+            arrow->updatePosition();
+        }
+    }
+
+    if(change == QGraphicsItem::ItemSelectedHasChanged){
+        if(DiagramScene* d_scene = dynamic_cast<DiagramScene*>(this->scene())){
+            d_scene->itemChanged(this);
+        }
+    }
+
+    return value;
 }
 
--- processitem.h
+++ processitem.h
@@ -9,12 +9,31 @@
 #include <QGraphicsItem>
 #include <QPainter>
 
+#include "arrowitem.h"
+
 class ProcessItem : public QGraphicsRectItem
 {
+    unsigned int current_value;
+    QList<ArrowItem*> arrows;
 public:
     enum {Type = UserType+1};
     ProcessItem(QGraphicsItem* parent = 0);
+    int type() const Q_DECL_OVERRIDE {return Type;}
+    void increaseValue();
+    void decreaseValue();
+    void setValue(unsigned int value);
+    unsigned int getValue();
+
+    void removeArrow(ArrowItem* arrow);
+    void removeArrows();
+    void addArrow(ArrowItem* arrow);
+
+    void updateItem();
     ~ProcessItem();
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) Q_DECL_OVERRIDE;
+signals:
+    void selectedChange(QGraphicsItem* item);
 };
 
 #endif // PROCESSITEM_H
